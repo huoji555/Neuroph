7581b6d601fa5610ae1982ba4fd3be448aeaa3a3
Ragty <961946407@qq.com>
2018-04-20 19:55:14 +0800

    新建框架

---
+++ src/main/java/hwj/nerouph/App.java
@@ -0,0 +1,13 @@
+package hwj.nerouph;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
